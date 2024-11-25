30c4e636c352dc71cd953f1ef947e7a752f82af9
Tayyab Mansoor <110661825+M-Tayyab15@users.noreply.github.com>
2024-11-25 08:57:03 +0500

    Add files via upload

---
+++ PHP + Many/day2/test.php
@@ -0,0 +1,25 @@
+<?php
+echo $_POST['name'];
+echo "<br>";
+echo $_POST['email'];
+echo "<br>";
+echo $_POST['fname'];
+echo "<br>";
+?>
+
+
+
+<?php
+echo $_POST['name1'];
+echo "<br>";
+echo $_POST['email1'];
+echo "<br>";
+echo $_POST['website1'];
+echo "<br>";
+echo $_POST['comment1'];
+echo "<br>";
+echo $_POST['gender'];
+echo "<br>";
+
+
+?>
