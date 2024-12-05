1628cf4b51813961a63dbfda6a5691d3fd9a3f36
Tayyab Mansoor <110661825+M-Tayyab15@users.noreply.github.com>
2024-12-05 08:54:36 +0500

    Add files via upload

---
+++ PHP + Many/Complete Project/Efooter.php
@@ -0,0 +1,53 @@
+<!-- Footer Start -->
+<footer class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <!-- Logo Section -->
+            <div class="col-lg-3 col-md-6 mb-4">
+                <a href="#" class="d-block mb-3">
+                    <img src="assets\Arsenal_FC.svg.png" alt="Logo" class="img-fluid" style="width: 120px;">
+                </a>
+                <p>Leading sportswear brand offering top-quality products for athletes.</p>
+            </div>
+
+            <!-- Quick Links -->
+            <div class="col-lg-3 col-md-6 mb-4">
+                <h5 class="text-uppercase">Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#shop" class="text-white">Shop</a></li>
+                    <li><a href="#about" class="text-white">About Us</a></li>
+                    <li><a href="#contact" class="text-white">Contact Us</a></li>
+                    <li><a href="#faq" class="text-white">FAQ</a></li>
+                </ul>
+            </div>
+
+            <!-- Categories Section -->
+            <div class="col-lg-3 col-md-6 mb-4">
+                <h5 class="text-uppercase">Categories</h5>
+                <ul class="list-unstyled">
+                    <li><a href="carousel.php#category-1" class="text-white">Football</a></li>
+                    <li><a href="carousel.php#category-2" class="text-white">Cricket</a></li>
+                    <li><a href="carousel.php#category-3" class="text-white">Tennis</a></li>
+                    <li><a href="carousel.php#category-4" class="text-white">Hockey</a></li>
+                    <li><a href="carousel.php#category-5" class="text-white">Badminton</a></li>
+                </ul>
+            </div>
+
+            <!-- Social Media Links -->
+            <div class="col-lg-3 col-md-6 mb-4">
+                <h5 class="text-uppercase">Follow Us</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white"><i class="bi bi-facebook"></i> Facebook</a></li>
+                    <li><a href="#" class="text-white"><i class="bi bi-twitter"></i> Twitter</a></li>
+                    <li><a href="#" class="text-white"><i class="bi bi-instagram"></i> Instagram</a></li>
+                    <li><a href="#" class="text-white"><i class="bi bi-youtube"></i> YouTube</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-4">
+        <div class="text-center">
+            <p>&copy; 2024 MTM_AFC. All Rights Reserved.</p>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
