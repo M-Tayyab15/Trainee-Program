daecf48f7548952ff7b8f320b871087283200d18
Tayyab Mansoor <110661825+M-Tayyab15@users.noreply.github.com>
2024-12-11 10:52:09 +0500

    Add files via upload

---
+++ PHP + Many/Complete Project/Layout/sidebar.php
@@ -0,0 +1,22 @@
+<nav id="sidebar" class="w3-sidebar w3-top w3-bottom w3-collapse w3-white w3-border-right w3-border-top scrollbar" style="z-index:3;width:230px;height:auto;margin-top:54px;border-color:rgba(0, 0, 0, .1)!important" id="mySidebar">
+  <div class="w3-bar-item w3-border-bottom w3-hide-large" style="padding:6px 0">
+    <label for="sidebar-control" class="w3-left w3-button w3-large w3-opacity-min" style="background:white!important"><i class="fa fa-bars"></i></label>
+    <h5 class="" style="line-height:1; margin:0!important; font-weight:300">
+      <a href="./index2.php" class="w3-button" style="background:white!important">
+        <img src="./assets/admin-logo.png" alt="w3 mix" class="w3-image"> &nbsp; W3Admin </a>
+    </h5>
+  </div>
+  <div class="w3-bar-block">
+    <span class="w3-bar-item w3-padding w3-small w3-opacity" style="margin-top:8px"> MAIN NAVIGATION </span>
+    <a href="./index2.php" class="w3-bar-item w3-button w3-padding-large w3-hover-text-primary">
+          <i class="fa fa-fw fa-bar-chart"></i>&nbsp; Dashboard </a>
+          <a href="./fileUpload.php" class="w3-bar-item w3-button w3-padding-large w3-hover-text-primary">
+          <i i class=" fa fa-fw fa-file-pdf-o"></i>&nbsp; File Upload</a>
+          <a href="./users.php" class="w3-bar-item w3-button w3-padding-large w3-hover-text-primary">
+          <i i class="fa fa-fw fa-users"></i>&nbsp; Manage Users</a>
+          <a href="./product.php" class="w3-bar-item w3-button w3-padding-large w3-hover-text-primary">
+          <i i class="fa fa-fw fa-shopping-cart"></i>&nbsp; Products</a>
+          <a href="./orders.php" class="w3-bar-item w3-button w3-padding-large w3-hover-text-primary">
+          <i i class="fa fa-fw fa-shopping-cart"></i>&nbsp; Orders</a>
+  </div>
+</nav>
